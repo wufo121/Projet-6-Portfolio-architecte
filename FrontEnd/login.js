a20b6f44fb84982610a2090b26b4f89b053d4155
wufo121 <wufo121@gmail.com>
2023-05-31 18:31:04 +0200

    ajout login fonctionnel

---
+++ FrontEnd/login.js
@@ -0,0 +1,36 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+  
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const data = {
+      email: email,
+      password: password
+    };
+  
+
+    fetch("http://localhost:5678/api/users/login", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    .then(function(response) {
+      if (response.ok) {
+        window.location.href = "index.html";
+      } else {
+        document.getElementById("login-form").classList.add("error")
+        console.log("Identifiants invalides. Veuillez réessayer.");
+
+        setTimeout(function() {
+            document.getElementById("login-form").classList.remove("error");
+          }, 500);
+      }
+    })
+    .catch(function(error) {
+      console.log("Une erreur s'est produite lors de la connexion :", error);
+    });
+  });
+  
+  
